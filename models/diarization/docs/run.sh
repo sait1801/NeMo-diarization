0ea8463f93f11c77be9637c9dfd0cce9c7f3ea56
Sait Burak Yucekaya <sait.burak20@gmail.com>
2024-10-30 18:18:59 +0300

    new folder structure

---
+++ models/diarization/docs/run.sh
@@ -0,0 +1,14 @@
+docker run --gpus all --rm \
+  -p8000:8000 -p8001:8001 -p8002:8002 \
+  -v /teamspace/studios/this_studio/NeMo-diarization/models:/models \
+  --shm-size=30g tritonn:latest \
+  tritonserver --model-repository=/models
+
+
+
+#   ----------------------NeMo--------------
+  docker run --gpus all --rm \
+  -p8000:8000 -p8001:8001 -p8002:8002 \
+  -v /teamspace/studios/this_studio/NeMo-diarization/server/diarization:/models \
+  --shm-size=30g tritonn:latest \
+  tritonserver --model-repository=/models
